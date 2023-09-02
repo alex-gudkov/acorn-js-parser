85ec5a49ff90f6658a69620dbedc4e3e3d024530
alex-gudkov <gudkov.bsuir@gmail.com>
2023-09-02 19:38:17 +0300

    add reading and writing file

--- src/main.js
+++ src/main.js
@@ -1,9 +1,29 @@
-var acorn = require('acorn');
+const acorn = require('acorn');
+const fs = require('fs/promises');
+const path = require('path');
 
-// parsed result is AST (abstract syntax tree) object as specified by ESTree spec
-var ast = acorn.parse('var x = 10, y = x;', {
-  ecmaVersion: 3,
-  sourceType: 'script',
-});
+async function main() {
+  try {
+    // read input file
+    const inputFileName = 'script.js';
+    const inputFilePath = path.join(__dirname, 'input', inputFileName);
+    const inputFileData = await fs.readFile(inputFilePath, { encoding: 'utf8' });
 
-console.dir(ast, { depth: Infinity });
+    // parsed result is AST (abstract syntax tree) object as specified by ESTree spec
+    const abstractSyntaxTree = acorn.parse(inputFileData, {
+      ecmaVersion: 2015,
+      sourceType: 'script',
+    });
+
+    // write output file
+    const outputFileName = 'script-ast.json';
+    const outputFilePath = path.join(__dirname, 'output', outputFileName);
+    const outputFileData = JSON.stringify(abstractSyntaxTree) + '\n';
+
+    await fs.writeFile(outputFilePath, outputFileData, { encoding: 'utf8' });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+main();
