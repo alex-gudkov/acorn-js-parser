44f3e9bb6759a86b711f3ee2e0d587aa868e38f5
alex-gudkov <gudkov.bsuir@gmail.com>
2023-09-02 19:46:09 +0300

    convert project to ES modules

--- src/main.js
+++ src/main.js
@@ -1,26 +1,26 @@
-const acorn = require('acorn');
-const fs = require('fs/promises');
-const path = require('path');
+import { parse } from 'acorn';
+import { readFile, writeFile } from 'fs/promises';
+import { resolve } from 'path';
 
 async function main() {
   try {
     // read input file
     const inputFileName = 'script.js';
-    const inputFilePath = path.join(__dirname, 'input', inputFileName);
-    const inputFileData = await fs.readFile(inputFilePath, { encoding: 'utf8' });
+    const inputFilePath = resolve('src', 'input', inputFileName);
+    const inputFileData = await readFile(inputFilePath, { encoding: 'utf8' });
 
     // parsed result is AST (abstract syntax tree) object as specified by ESTree spec
-    const abstractSyntaxTree = acorn.parse(inputFileData, {
+    const abstractSyntaxTree = parse(inputFileData, {
       ecmaVersion: 2015,
       sourceType: 'script',
     });
 
     // write output file
     const outputFileName = 'script-ast.json';
-    const outputFilePath = path.join(__dirname, 'output', outputFileName);
+    const outputFilePath = resolve('src', 'output', outputFileName);
     const outputFileData = JSON.stringify(abstractSyntaxTree) + '\n';
 
-    await fs.writeFile(outputFilePath, outputFileData, { encoding: 'utf8' });
+    await writeFile(outputFilePath, outputFileData, { encoding: 'utf8' });
   } catch (error) {
     console.error(error);
   }
